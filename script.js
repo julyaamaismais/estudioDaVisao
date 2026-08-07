e57782baf46edb9cf8d27d1193a4f786765c1576
julyaamaismais <julya.queiroz@escola.pr.gov.br>
2026-08-07 08:58:23 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+//Botoes de acessibilidades 
+document.addEventListener("DOMContentLoaded,() =>{
+    const btnContraste = document.getElementById("btn-contraste");
+    const btnAumentar = document.getElementById("btn-aumentar-texto");
+    const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+    //aumentar a fonte
+
+    let tamanhoAtualFonte = 100;
+
+    //função alto contraste
+
+    btnContraste.addEventListener("click",() ) =>{
+
+    document.body.
+
+    )
+}
