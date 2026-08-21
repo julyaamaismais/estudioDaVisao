abf35a6d79a753c4ce112bfd47d1856294764fb1
julyaamaismais <julya.queiroz@escola.pr.gov.br>
2026-08-21 11:28:59 -0300

    ok

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-//Botoes de acessibilidades 
-document.addEventListener("DOMContentLoaded,() =>{
+// Botões de acessibilidade 
+document.addEventListener("DOMContentLoaded", () => {
     const btnContraste = document.getElementById("btn-contraste");
     const btnAumentar = document.getElementById("btn-aumentar-texto");
     const btnDiminuir = document.getElementById("btn-diminuir-texto");
 
-    //aumentar a fonte
-
     let tamanhoAtualFonte = 100;
 
-    //função alto contraste
+    // Função alto contraste
+    if (btnContraste) {
+        btnContraste.addEventListener("click", () => {
+            document.body.classList.toggle("alto-contraste");
 
-    btnContraste.addEventListener("click",() ) =>{
+            // Acessibilidade para leitores de tela
+            const ativo = document.body.classList.contains("alto-contraste");
+            btnContraste.setAttribute("aria-pressed", ativo);
+        });
+    }
 
-    document.body.classList.toogle("alto-contraste");
+    // Função aumentar texto
+    if (btnAumentar) {
+        btnAumentar.addEventListener("click", () => {
+            if (tamanhoAtualFonte < 150) {
+                tamanhoAtualFonte += 10;
+                document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+            }
+        });
+    }
 
-    //acessibilidade para leitores de tela
-
-    const ativas = document.body.classList.contains("alto-contrastes");
-    btnContrast.setAttibute("aria-pressed",ativo);
-
-    )};
-
-    //função aumentar texto
-
-    btnAumentar.addEventListener("click",()=>{
-        if(tamanhoAtualFonte <150){
-            tamanhoAtualFonte += 10;
-            document.documentElement.style.fontSize
-             `${tamanhoAtualFonte}%`;
-        }
-    });
-      //função diminuir texto;
-      btnDiminuir.addEventListener("click", ()=>{
-        if(tamanhoAtualFonte > 90){
-            tamanhoAtualFonte -=10;
-            document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
-        }
-      })
+    // Função diminuir texto
+    if (btnDiminuir) {
+        btnDiminuir.addEventListener("click", () => {
+            if (tamanhoAtualFonte > 90) {
+                tamanhoAtualFonte -= 10;
+                document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+            }
+        });
+    }
+});
